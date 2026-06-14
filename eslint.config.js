8de8fa8cc4ba0a4d5f91e8a741d4e7f8c3083e7e
Ryan Novinc <ryannovinc@Mac.lan>
2026-06-14 20:10:17 +1000

    Ignore DateDownMobile in ESLint (excluded from tsconfig, parser errors)

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,6 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*', 'ios/*', 'android/*', '.expo/*'],
+    ignores: ['dist/*', 'ios/*', 'android/*', '.expo/*', '**/DateDownMobile/**'],
   },
 ]);
